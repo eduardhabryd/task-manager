80c0a69109c64ffc8f3e56db959c7d5256cba230
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-05 12:28:32 +0300

    add task create view and task update view

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
+from tasks.forms import TaskCreateForm
 from tasks.models import Task, Tag
 
 
@@ -9,5 +10,15 @@ class TaskListView(generic.ListView):
 	template_name = "tasks/index.html"
 
 
+class TaskCreateView(generic.CreateView):
+	model = Task
+	form_class = TaskCreateForm
+
+class TaskUpdateView(generic.UpdateView):
+	model = Task
+	fields = "__all__"
+
+
 class TagListView(generic.ListView):
 	model = Tag
+
