1fed339f7973eaf712e630997afc1c9dd0d2472e
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-05 11:57:51 +0300

    add tag list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 
-from tasks.models import Task
+from tasks.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
 	model = Task
 	template_name = "tasks/index.html"
+
+
+class TagListView(generic.ListView):
+	model = Tag
