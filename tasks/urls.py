d3a1a0409a06f2bedd891dfca9c58e1005a027cb
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-05 12:28:04 +0300

    add tasks/create and tasks/<int:pk>/update paths

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,6 +7,8 @@ from tasks import views
 urlpatterns = [
 	path("", views.TaskListView.as_view(), name="index"),
 	path("tags/", views.TagListView.as_view(), name="tags"),
+	path("tasks/create/", views.TaskCreateView.as_view(), name="create-task"),
+	path("tasks/<int:pk>/update", views.TaskUpdateView.as_view(), name="task-update"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "tasks"
