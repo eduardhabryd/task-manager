e2e681b4e57f7f4aa58a608484c0f751cc1daba2
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-05 13:40:37 +0300

    add TagCreateView and TagUpdateView

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from tasks.forms import TaskCreateForm
@@ -13,12 +14,26 @@ class TaskListView(generic.ListView):
 class TaskCreateView(generic.CreateView):
 	model = Task
 	form_class = TaskCreateForm
+	success_url = reverse_lazy("tasks:index")
+
 
 class TaskUpdateView(generic.UpdateView):
 	model = Task
 	fields = "__all__"
+	success_url = reverse_lazy("tasks:index")
 
 
 class TagListView(generic.ListView):
 	model = Tag
 
+
+class TagCreateView(generic.CreateView):
+	model = Tag
+	fields = "__all__"
+	success_url = reverse_lazy("tasks:tags")
+
+
+class TagUpdateView(generic.UpdateView):
+	model = Tag
+	fields = "__all__"
+	success_url = reverse_lazy("tasks:tags")
