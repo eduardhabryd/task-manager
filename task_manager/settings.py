65420d20c25ce6badef98518ffda9cb99ca0b8e5
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-05 12:27:20 +0300

    configure crispy forms

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -15,8 +15,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'crispy_forms',
+    "crispy_bootstrap4",
     "tasks",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
